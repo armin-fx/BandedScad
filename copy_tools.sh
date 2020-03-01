a3a2e482d4b410d85ad706f9020bf50dd62d0a62
armin-fx <rechner_x@yahoo.de>
2020-03-01 19:39:01 +0100

    add simple script to copy tools files into OpenSCAD libraries folder

---
+++ copy_tools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+OpenSCAD_lib=~/.local/share/OpenSCAD/libraries
+tools=.
+
+if [ -d "$OpenSCAD_lib" ]; then
+	if [ -d 	 "$OpenSCAD_lib/tools" ]; then
+		rm -r    "$OpenSCAD_lib/tools/"*
+	else
+		mkdir -p "$OpenSCAD_lib/tools"
+	fi
+	cp "$tools/tools/"*    "$OpenSCAD_lib/tools"
+	cp "$tools/tools.scad" "$OpenSCAD_lib"
+else
+	echo "OpenSCAD libraries folder not found."
+fi
